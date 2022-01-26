62500785f68734acfcf160d37685bd44dd28068f
EC2 Default User <ec2-user@ip-172-31-11-79.ec2.internal>
2022-01-26 21:39:08 +0000

    Lec2

---
+++ Lec2.py
@@ -0,0 +1,51 @@
+"""
+this is a regional comment
+
+lec 2
+
+"""
+
+#print("hello world") # this is a line comment
+
+# print(type(123))
+
+# print(type(123.))
+
+# print(type("123"))
+
+# print( "hello world" )
+# print("hello world")
+
+# print( "Tom's cat" )
+
+# print("Hello World")
+
+# print ("Hello World".upper())
+
+# print("Hello World".lower())
+
+# print("  Hello Word  ")
+# print("  Hello Word  " .strip())
+
+# print("Hello Word".split())
+# print("Hello, World".split(','))
+
+# print("Hello, World".split("o"))
+
+# print(2. ** 3)
+
+my_int = 123
+
+print(my_int)
+
+my_int = 321
+
+print(my_int)
+
+my_float = 123.
+
+my_str = "hello world"
+
+print(my_float + 1)
+
+print(my_str.upper())

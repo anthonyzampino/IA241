34f095998a9d8f828451477d49635df7568adf87
EC2 Default User <ec2-user@ip-172-31-11-79.ec2.internal>
2022-03-02 01:03:57 +0000

    lab5

---
+++ lec5.py
@@ -0,0 +1,73 @@
+'''
+lec5 if statement
+'''
+
+import this
+
+print(  2+
+        1)
+        
+print([  1,2,3,
+        4,5,6])
+
+m = 1 +\
+         2
+print(m)
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(id([1,2,3]))
+print(id(a))
+print(id(b))
+
+print(a is b)
+print(a == b)
+
+
+x = None
+
+print(id(x))
+print(id(None))
+
+print(x is None)
+print(x == None)
+
+y = []
+
+print(y == None)
+print(y is None)
+
+print(True and False)
+print(True or False)
+print(not True)
+print(not False)
+
+
+print(not None)
+print(not '0')
+
+if 2>1 :
+
+  print('2>1')
+  
+  if 3>1:
+      print('3>1')
+
+  if 2<1:
+      print('2<1')
+
+print('not in the if block')
+
+
+if 2<=2:
+        print('2<=2')
+
+if 2<1:
+        print('2>1')
+
+print(() and [])
+print([] and ())
+
+print(-1 or 0)
+print(0 or -1)
